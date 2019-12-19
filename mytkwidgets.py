7b4b16cf0df58fa4cae51994ce512675ab461ef3
SpringVaS <valentin.springsklee@student.kit.edu>
2019-12-19 12:41:51 +0100

    Created GUI with Date and Time Selectors

---
+++ mytkwidgets.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+# found at https://stackoverflow.com/questions/57034118/time-picker-for-tkinter 19.12.2019
+
+class TimeEntry(tk.Frame):
+    def __init__(self, parent, font = None):
+        super().__init__(parent)
+        self.hourstr=tk.StringVar(self,'10')
+        # TO DO sanitize entry
+        self.hour = tk.Spinbox(self,from_=0,to=23,wrap=True,textvariable=self.hourstr,width=2,font = font)
+        self.minstr=tk.StringVar(self,'30')
+        self.minstr.trace("w",self.trace_var)
+        self.last_value = ""
+        self.min = tk.Spinbox(self,from_=0,to=59,wrap=True,textvariable=self.minstr,width=2,font = font)
+        self.hour.grid()
+        self.min.grid(row=0,column=1)
+
+    def trace_var(self,*args):
+        print('change')
+        if self.last_value == "59" and self.minstr.get() == "0":
+            self.hourstr.set(int(self.hourstr.get())+1 if self.hourstr.get() !="23" else 0)
+        self.last_value = self.minstr.get()
+
+    def getSelectedTime(self):
+        timestr = self.hourstr + ':' + self.minstr
+        return timestr
--- parameterui.py
+++ parameterui.py
@@ -1,8 +1,9 @@
 import queryseatfinder as qsf
 
-import tkinter
+import tkinter as tk
 from tkinter import ttk
 import tkcalendar
+import mytkwidgets as mw
 
 import pandas as pd
 import numpy as np
@@ -14,33 +15,43 @@ class ViewController(object):
 		self.__buildGUI()
 
 	def __buildGUI(self):
-		self.window = tkinter.Tk()
+		self.window = tk.Tk()
 		self.window.title("Query Seatfinder")
-		self.window.geometry('400x640')
 
-		label = tkinter.Label(self.window, text = "Please enter a range", font = ('Segoe UI', 18))
-		label.grid(row = 0, column = 0)
-		button_send_query = tkinter.Button(self.window, text = 'OK', command = self.buttonPressed)
-		button_send_query.grid(row = 2, column = 0)
+		guiPane = tk.Frame(self.window)
 
-		dateFrom = tkcalendar.DateEntry(self.window, width=12, background='darkblue', 
-			foreground='white', borderwidth=2)
-		dateFrom.grid(row = 1, column = 0)
+		label = tk.Label(guiPane, text = "Select a period of time", font = ('Segoe UI', 12))
+		label.grid(row = 0, columnspan = 2, padx = 10, pady = 10)
+		button_send_query = tk.Button(guiPane, text = 'OK', command = self.buttonPressed)
+		button_send_query.grid(row = 3, column = 0, columnspan = 2, padx = 10, pady = 10, sticky = 'we')
 
-		dateTo = tkcalendar.DateEntry(self.window, width=12, background='darkblue', 
-			foreground='white', borderwidth=2)
+		self.dateEntryFrom = tkcalendar.DateEntry(guiPane, width=12, background='darkblue', 
+			foreground='white', borderwidth=2, font = ('Segoe UI', 12))
+		self.dateEntryTo = tkcalendar.DateEntry(guiPane, width=12, background='darkblue', 
+			foreground='white', borderwidth=2, font = ('Segoe UI', 12))
 
-		dateTo.grid(row = 1, column = 1)
+		self.timeEntryFrom = mw.TimeEntry(guiPane, font = ('Segoe UI', 12))
+		self.timeEntryTo = mw.TimeEntry(guiPane, font = ('Segoe UI', 12))
+
+		self.dateEntryFrom.grid(row = 1, column = 0, padx = 10, pady = 10)
+		self.timeEntryFrom.grid(row = 1, column = 1, padx = 10, pady = 10)
+		self.dateEntryTo.grid(row = 2, column = 0, padx = 10, pady = 10)
+		self.timeEntryTo.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+		guiPane.pack()
 
 		self.window.mainloop()
 
 
 	def buttonPressed(self):
+
+		print(self.dateEntryFrom.get_date())
+
 		timebegin = pd.Timestamp('2018-10-15 07:00:00')
 		timeend   = pd.Timestamp('2018-10-15 22:15:00')
 
-		occupancy = self.model.getInfo('seatestimate',timebegin, timeend)
-		self.model.writeInfoToExcel(occupancy, "Seat Occupancy")
+		#occupancy = self.model.getInfo('seatestimate',timebegin, timeend)
+		#self.model.writeInfoToExcel(occupancy, "Seat Occupancy")
 
 
 m = qsf.Model(qsf.URL, 'data.xlsx')
